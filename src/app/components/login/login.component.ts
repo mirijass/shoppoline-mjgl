938881f082198adfc69ed0f612df4bac391e08f2
Miriam Guerrero <miriamlozano204@gmail.com>
2025-03-15 21:44:37 -0600

    .

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,30 +1,38 @@
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ModprimengModule } from '../../modprimeng.module';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, ModprimengModule, RouterModule],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, ModprimengModule, RouterModule], // Agrega CommonModule aquí
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForma: FormGroup;
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb: FormBuilder) {
     this.loginForma = this.fb.group({
-      email: ['', [Validators.required, Validators.email] ],
-      password: ['', [Validators.required]]
-    })
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(/^(?=.[a-z])(?=.[A-Z])(?=.*[^A-Za-z0-9]).{8,}$/),
+        ],
+      ],
+    });
   }
 
-  get email(){
+  get email() {
     return this.loginForma.controls['email'];
   }
 
-  get password(){
+  get password() {
     return this.loginForma.controls['password'];
   }
-
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ModprimengModule } from '../../modprimeng.module';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, ModprimengModule, RouterModule],
+  imports: [ReactiveFormsModule, ModprimengModule, RouterModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   registerForma: FormGroup;
+email: any;
+password: any;
+fullName: any;
+confirmPassword: any;
 
   constructor(private fb:FormBuilder){
     this.registerForma = this.fb.group({
